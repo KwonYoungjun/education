053b2cefca262e622444c2d0cfdf57956cea7275
user-PC\user <kyj9107@naver.com>
2017-02-02 11:06:57 +0900

    second commit work

--- HellogGit/src/test/HelloJava.java
+++ HellogGit/src/test/HelloJava.java
@@ -6,6 +6,9 @@ public class HelloJava {
 		// TODO Auto-generated method stub
 		System.out.println("hello java~~~");
 		System.out.println("first commit work");
+		
+		// branch 추가 내용
+		System.out.println();
 	}
 
 }
