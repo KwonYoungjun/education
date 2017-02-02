8a068b5ad70973bd35bb77170ffc17d692738fe0
user-PC\user <kyj9107@naver.com>
2017-02-02 11:44:02 +0900

    Third commit work

--- HellogGit/src/test/HelloJava.java
+++ HellogGit/src/test/HelloJava.java
@@ -8,7 +8,7 @@ public class HelloJava {
 		System.out.println("first commit work");
 		
 		// branch 추가 내용
-		System.out.println("Second commit work");
+		System.out.println("Second commit work..");
 		
 		// 지역 저장소 내용 추가
 		System.out.println("Third commit work");
