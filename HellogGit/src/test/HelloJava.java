ce6c63f031ca0bcfd1fa8091d31956135e82b7f2
user-PC\user <kyj9107@naver.com>
2017-02-02 10:47:08 +0900

    create java project first commit work

---
+++ HellogGit/src/test/HelloJava.java
@@ -0,0 +1,11 @@
+package test;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java~~~");
+		System.out.println("first commit work");
+	}
+
+}
